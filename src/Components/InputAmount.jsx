02c2df051ab5c6af7fdacb4c84788ebf37597fde
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-17 14:24:02 +0100

    Cooking...

--- src/Components/InputAmount.jsx
+++ src/Components/InputAmount.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const InputAmount = () => {
     return (
-        <div>InputAmount</div>
+        <div>
+            <input
+                type="number"
+                // value={Title}
+                // onChange={(e) => setTitle(e.target.value)}
+                placeholder="Add an amount"
+                className="p-2 text-xl font-bold transition-all duration-500 border-b-2 border-gray-200 outline-none sm:text-2xl dark:bg-transparent dark:border-2 dark:rounded-lg dark:border-white"
+            />
+        </div>
     )
 }
 
