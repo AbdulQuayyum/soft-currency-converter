6f91aeb440fae731b2dfc0bb1c445d21d9f04e46
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-17 14:10:25 +0100

    Cooking...

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -1,4 +1,7 @@
 import Footer from "./Footer";
+import InputAmount from "./InputAmount";
 import Navbar from "./Navbar";
+import SelectCountry from "./SelectCountry";
+import SwitchCurrency from "./SwitchCurrency";
 
-export { Footer, Navbar }
+export { Footer, InputAmount, Navbar, SelectCountry, SwitchCurrency }
