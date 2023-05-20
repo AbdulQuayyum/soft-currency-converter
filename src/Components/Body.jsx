4988c7853678180d93852fb3a746c5af7eae300e
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-20 12:44:59 +0100

    Cooking...

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -5,7 +5,9 @@ import { InputAmount, SelectCountry, SwitchCurrency } from "./Index"
 const Body = () => {
     return (
         <div>
-            <div className='flex items-center gap-6 flex-col lg:flex-row px-6 py-6 sm:py-12 sm:px-20 border-black border rounded-lg'>
+            <div
+            style={{ boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1)" }}
+             className='flex items-center gap-6 flex-col lg:flex-row px-6 py-6 sm:py-24 sm:px-20 border-black border rounded-lg'>
                 <InputAmount />
                 <SelectCountry />
                 <SwitchCurrency />
