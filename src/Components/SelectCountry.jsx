3fa5af0c6d73b6e013dc1f5df5869cbb36860aa9
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-20 12:15:26 +0100

    Cooking...

--- src/Components/SelectCountry.jsx
+++ src/Components/SelectCountry.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Select from "react-select"
 
-const SelectCountry = () => {
+const SelectCountry = (props) => {
+    const [isClearable, setIsClearable] = useState(true)
+    const [isSearchable, setIsSearchable] = useState(true)
+
+    const customStyles = {
+        control: (provided) => ({
+            ...provided,
+            cursor: "pointer",
+            background: '#F7F7F7',
+            borderColor: "transparent",
+            borderRadius: "12px",
+            minHeight: '48px',
+            padding: "0px 10px"
+        })
+    };
+
     return (
-        <div>SelectCountry</div>
+        <div>
+            <Select
+                styles={customStyles}
+                className="basic-single"
+                classNamePrefix="select"
+                //   defaultValue={colourOptions[0]}
+                isClearable={isClearable}
+                isSearchable={isSearchable}
+                name="countries"
+            //   options={colourOptions}
+            />
+        </div>
     )
 }
 
