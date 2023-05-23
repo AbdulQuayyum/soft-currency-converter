855ae33d969f107ddac5c443fd7504691d2aba81
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-23 17:30:43 +0100

    Chief Cooker Cooking...

--- src/Components/InputAmount.jsx
+++ src/Components/InputAmount.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import { useContext } from "react"
+import { CurrencyContext } from "../Contexts/CurrencyContext"
 
 const InputAmount = () => {
+    const { firstAmount, setFirstAmount } = useContext(CurrencyContext)
+
     return (
         <div>
             <input
                 type="number"
+                value={firstAmount}
+                onChange={e => setFirstAmount(e.target.value)}
                 // value={Title}
                 // onChange={(e) => setTitle(e.target.value)}
                 placeholder="Add an amount"
